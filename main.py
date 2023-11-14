66cf16a3e090138faacaf685ee9c115511a14543
VovaDEU <150560632+VovaDEU@users.noreply.github.com>
2023-11-14 11:15:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 import random
 import PySimpleGUI as sg
-target_number = random.randint(1, 100)
-def guess_number(values1,target_number):
-    values1 = int(values1)
 
+class NumberGuesser:
+    def __init__(self, target_number):
+        self.target_number = target_number
 
-    try:
+    def guess_number(self, user_guess):
+        try:
+            user_guess = int(user_guess)
 
+            if user_guess < self.target_number:
+                print("Загадане число більше.")
+            elif user_guess > self.target_number:
+                print("Загадане число менше.")
+            else:
+                print(f"Вітаємо! Ви вгадали число {self.target_number}")
 
-        if values1< target_number:
-            print("Загадане число більше.")
-        elif values1> target_number:
-            print("Загадане число менше.")
-        else:
-            print(f"Вітаємо! Ви вгадали число {target_number}")
+        except ValueError:
+            print("Будь ласка, введіть ціле число.")
 
-    except ValueError:
-        print("Будь ласка, введіть ціле число.")
-sg.theme('DarkAmber')   # Add a touch of color
-layout = [  [sg.Text('Спробуйте вгадати число від 1 до 100:')],
+class InputHandler:
+    def get_user_input(self):
+        layout = [
+            [sg.Text('Спробуйте вгадати число від 1 до 100:')],
             [sg.Text('Вгадайте число'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-window = sg.Window('Window Title', layout)
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
+            [sg.Button('Ok'), sg.Button('Cancel')]
+        ]
 
-    guess_number(values[0],target_number)
+        window = sg.Window('Window Title', layout)
+        event, values = window.read()
+        window.close()
 
+        return values[0] if event != sg.WIN_CLOSED and event != 'Cancel' else None
 
-window.close()
+def main():
+    target_number = random.randint(1, 100)
+    input_handler = InputHandler()
+    game = NumberGuesser(target_number)
+
+    while True:
+        user_input = input_handler.get_user_input()
+
+        if user_input is None:
+            break
+
+        game.guess_number(user_input)
+
+if __name__ == "__main__":
+    main()
